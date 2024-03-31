f14a978f67866e364421f8558eaab77b3693b029
a.mohebbi <alimohebbise@gmail.com>
2024-03-31 18:55:14 +0330

    first commit

---
+++ src/main/java/package1/MainClass1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class MainClass1 {
+
+    public static void main(String[] args) {
+        int i = 6;
+        int j = 10;
+        for (int k = 0; k <= 6; k++){
+            System.out.println(k);
+        }
+    }
+}
---
+++ src/main/java/package1/MainClass2.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class MainClass2 {
+    int i = 0;
+    int j = 6;
+    public static void main(String[] args){
+
+    }
+}
